3825410890ecb03c29da43ad6bbf10da048baed1
Arqade <96065790+ArqadeTV@users.noreply.github.com>
2026-08-28 15:25:21 -0500

    Improve timer functionality in script.js
    Refactor timer logic to update title index every second.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var title_index=document.querySelector(".title_index-html");
 var titleNum = 0;
-setInterval(function(){
+title_index.innerHTML=String(titleNum)+" Seconds!";
+setTimeout(function(){
   titleNum++;
   title_index.innerHTML=String(titleNum)+" Second!";
+  setInterval(function(){
+    titleNum++;
+    title_index.innerHTML=String(titleNum)+" Second!";
+  }, 1000);
 }, 1000);
